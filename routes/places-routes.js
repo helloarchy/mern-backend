0eab02d2247184a371844120591e12fd77e81615
Robert Hardy <helloarchy@gmail.com>
2020-11-22 15:41:43 +0000

    Basic setup in place, with / GET route for landing page.

---
+++ routes/places-routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  console.log('GET Request in places');
+  res.json({
+    message: 'It works!'
+  })
+})
+
+module.exports = router
