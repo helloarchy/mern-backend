dc89c8a0f4808edd4f84b424db83614c6475ddbc
Robert Hardy <helloarchy@gmail.com>
2020-12-06 14:47:20 +0000

    Error handling in the places routes.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// Const bodyParse = require('body-parser')
+const bodyParse = require('body-parser')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
@@ -8,5 +8,14 @@ const app = express();
 
 app.use('/api/places', placesRoutes)
 app.use('/api/users', usersRoutes)
+
+app.use((err, req,res,next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+
+  res.status(err.code || 500);
+  res.json({message: err.message || 'An unknown error occurred.' });
+});
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,54 +4,58 @@ const express = require('express');
 const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-        'address': 'Tower Bridge Rd, London SE1 2UP',
-        'creator': 'u1',
-        'description': 'A nice tower description',
-        'id': 'p1',
-        'imageUrl': 'https://www.swedishnomad.com/wp-content/images/2020/03/Tower-Bridge.jpg',
-        'location': {
-            'lat': 51.505455,
-            'lng': -0.075356
-        },
-        'title': 'Tower Bridge'
+  {
+    'address': 'Tower Bridge Rd, London SE1 2UP',
+    'creator': 'u1',
+    'description': 'A nice tower description',
+    'id': 'p1',
+    'imageUrl': 'https://www.swedishnomad.com/wp-content/images/2020/03/Tower-Bridge.jpg',
+    'location': {
+      'lat': 51.505455,
+      'lng': -0.075356,
     },
-    {
-        'address': 'Tower Bridge Rd, London SE1 2UP',
-        'creator': 'u2',
-        'description': 'An even nicer tower description',
-        'id': 'p2',
-        'imageUrl': 'https://www.swedishnomad.com/wp-content/images/2020/03/Tower-Bridge.jpg',
-        'location': {
-            'lat': 51.505455,
-            'lng': -0.075356
-        },
-        'title': 'Tower Bridge'
-    }
+    'title': 'Tower Bridge',
+  },
+  {
+    'address': 'Tower Bridge Rd, London SE1 2UP',
+    'creator': 'u2',
+    'description': 'An even nicer tower description',
+    'id': 'p2',
+    'imageUrl': 'https://www.swedishnomad.com/wp-content/images/2020/03/Tower-Bridge.jpg',
+    'location': {
+      'lat': 51.505455,
+      'lng': -0.075356,
+    },
+    'title': 'Tower Bridge',
+  },
 ];
 
-router.get('/:pid', (req, res) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find((p) => p.id === placeId);
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
 
-    // eslint-disable-next-line no-console
-    console.log('GET /:pid Request in places');
-    res.json({
-        'message': 'It works?',
-        place
-    });
+  if (!place) {
+      const error = new Error(`Could not find a place with id ${placeId}.`)
+      error.code = 404;
+      throw error;
+  }
+
+  console.log('GET /:pid Request in places');
+  res.json({place});
 });
 
-router.get('/user/:uid', (req, res) => {
-    const userId = req.params.uid;
-    const place = DUMMY_PLACES.find((p) => p.creator === userId);
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => p.creator === userId);
 
-    // eslint-disable-next-line no-console
-    console.log('GET /user/:uid Request in places');
-    res.json({
-        'message': 'It works?',
-        place
-    });
+  if (!place) {
+      const error = new Error(`Could not find a place with user id ${userId}.`)
+      error.code = 404;
+      return next(error);
+  }
+
+  console.log('GET /user/:uid Request in places');
+  res.json({place});
 });
 
 module.exports = router;
