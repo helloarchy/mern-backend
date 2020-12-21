ddf7dc7a3f090e2894689eafbe26784c84896050
Robert Hardy <helloarchy@gmail.com>
2020-12-21 15:46:14 +0000

    Throw error when route is unrecognised/invalid

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const bodyParser = require('body-parser')
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
 
-const app = express();
+const HttpError = require('./models/http-error')
 
 /**
  * Middleware loaded top-down!
  * Parse JSON before routing requests
  * Then next, next, next to use following middleware
  */
+const app = express();
 app.use(bodyParser.json())
 app.use('/api/places', placesRoutes)
 app.use('/api/users', usersRoutes)
+
+app.use((req, res, next) => {
+  throw new HttpError('Could not find this route', 404)
+})
 
 app.use((err, req,res,next) => {
   if (res.headerSent) {
